e04fc965f14db6e71ce84aac538364a77c5ac5ec
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2021-06-03 14:53:33 -0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+const navSlide = () => {
+  const burger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("is-active");
+    //Animate Links
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade .1s ease-in forwards`;
+      }
+    });
+    //burger animation
+    burger.classList.toggle("is-active");
+  });
+};
+navSlide();
